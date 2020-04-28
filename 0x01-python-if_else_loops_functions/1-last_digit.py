3c0706212f63e6e97b0468b5bb673ed1b10ad015
JuanJoseGomezR <juangomez0149@gmail.com>
2020-04-28 18:34:55 +0000

    [Fixed] Indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ if number % 10 > 5:
     print("Last digit of {:d}".format(number) +
           " is {:d} and is greater than 5".format(number))
 elif number % 10 == 0:
-    print("Last digit of {:d}".format(number) + " is\
-          {:d} and is 0".format(number))
+    print("Last digit of {:d}".format(number) + " is {:d} and is 0"
+          .format(number))
 elif number % 10 < 6 and number % 10 != 0:
-    print("Last digit of {:d}".format(number) + " is\
-          {:d} and is less than 6 and not 0".format(number))
+    print("Last digit of {:d}".format(number) +
+          " is {:d} and is less than 6 and not 0".format(number))
