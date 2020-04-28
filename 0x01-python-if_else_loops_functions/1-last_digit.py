203c92e63069665196be47b23dbf4c778ef0cefb
JuanJoseGomezR <juangomez0149@gmail.com>
2020-04-28 18:27:30 +0000

    [Fixed] Pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number >= 0:
-    ld = number % 10
-if ld > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5"
-          .format(number, ld))
-elif ld == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, ld))
-elif ld < 6:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
-          .format(number, ld))
-if number < 0:
-    ld = number % -10
-if ld > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5"
-          .format(number, ld))
-elif ld == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, ld))
-elif ld < 6:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
-          .format(number, ld))
+if number % 10 > 5:
+    print("Last digit of {:d}".format(number) +
+          " is {:d} and is greater than 5".format(number))
+elif number % 10 == 0:
+    print("Last digit of {:d}".format(number) + " is\
+          {:d} and is 0".format(number))
+elif number % 10 < 6 and number % 10 != 0:
+    print("Last digit of {:d}".format(number) + " is\
+          {:d} and is less than 6 and not 0".format(number))
