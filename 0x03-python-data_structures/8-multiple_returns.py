cfd3102e95f13f26f719d30785d7f122ce0f72fa
JuanJoseGomezR <juangomez0149@gmail.com>
2020-05-05 23:35:49 +0000

    [Fixed] Indentation

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,4 @@ def multiple_returns(sentence):
     if sentence is not None:
         if len(sentence) == 0:
             return(0, None)
-        return len(sentence), sentence[0]s
+        return len(sentence), sentence[0]
