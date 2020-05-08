8813f9bfc6ffbcebcbd3a6689e0c6edb187cc7be
JuanJoseGomezR <juangomez0149@gmail.com>
2020-05-08 00:22:14 +0000

    [Fixed] Missclicks

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     new_list = sorted(list(a_dictionary.items()))
     for i, j in new_list:
-        print(i + ': ' + str(j))GI
+        print(i + ': ' + str(j))
