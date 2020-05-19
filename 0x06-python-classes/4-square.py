e012ab6a8a95ba73039bcf47dc6410d7321ecabf
JuanJoseGomezR <juangomez0149@gmail.com>
2020-05-19 21:17:51 +0000

    [Uploading] Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""define square"""
+
+
+class Square:
+    """Initialize"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        """attribute"""
+        self.__size = size
+
+    def size(self):
+        """Retrieves itself"""
+        return self.__size
+
+    def area(self):
+        """area of the square"""
+        return self.__size**2
+
+    def size(self, value):
+        """sets area of square"""
+        if not type(value) == int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
