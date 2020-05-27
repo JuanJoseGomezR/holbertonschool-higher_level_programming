80ddad708cf5021062588626b3158f8f70bbce5d
JuanJoseGomezR <juangomez0149@gmail.com>
2020-05-27 04:27:27 +0000

    [Fixed] Code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+defines locked class
+"""
+
+
 class LockedClass:
-    __slots__ = 'first_name'
+    """prev form creating new
+    """
+    __slots__ = ['first_name']
+
+    def __init__(self, first_n=""):
+        self.first_name = first_n
