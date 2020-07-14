5b2746790f30c8d8c3a5d2abcf7073a502062922
JuanJoseGomezR <juangomez0149@gmail.com>
2020-07-14 18:03:47 +0000

    [Uploading] Task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- list all the privileges of the MySQL users
--- list privileges
+--  Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost)
+-- FLUSH PRIVILEGES;
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
