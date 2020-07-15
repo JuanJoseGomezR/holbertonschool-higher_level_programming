50a7fddbd78ff3eb9eac7a12a75f36e38c3ecc24
JuanJoseGomezR <juangomez0149@gmail.com>
2020-07-15 00:11:36 +0000

    [Uploading] More Tasks

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,12 @@
+-- atabase dump from hbtn_0d_tvshows to your MySQL serve
+SELECT
+	tv_shows.title, tv_show_genres.genre_id
+from
+	tv_shows
+JOIN
+	tv_show_genres
+WHERE
+	tv_shows.id = tv_show_genres.show_id
+ORDER BY
+	tv_shows.title, tv_show_genres.genre_id
+ASC;
