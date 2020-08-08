1fdda62a2ee2e887321ed1e9ad9424eb1b9a6ff1
JuanJoseGomezR <juangomez0149@gmail.com>
2020-08-08 16:52:53 +0000

    [Fixed] Pep8: lines 12 & 13 indent

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,8 +9,8 @@ import MySQLdb
 if __name__ == "__main__":
     """ not executed when imported """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                        .format(argv[1], argv[2], argv[3]),
-                        pool_pre_ping=True)
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
