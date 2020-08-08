0233647cf9d001cd3bc9209da0d0bdcf6631bb9c
JuanJoseGomezR <juangomez0149@gmail.com>
2020-08-08 16:51:37 +0000

    [Fixed] Executed when imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,13 +6,14 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 import MySQLdb
 
-
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(argv[1], argv[2], argv[3]),
-                       pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-for i in session.query(State).order_by(State.id):
-    print("{}: {}".format(i.id, i.name))
-session.close()
+if __name__ == "__main__":
+    """ not executed when imported """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                        .format(argv[1], argv[2], argv[3]),
+                        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for i in session.query(State).order_by(State.id):
+        print("{}: {}".format(i.id, i.name))
+    session.close()
