02c5185e7f9796d854348222eaf2d2fa1cdf6f0d
JuanJoseGomezR <juangomez0149@gmail.com>
2020-08-08 16:03:17 +0000

    [Fixed] Pep8: Trailing white space

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
                          host="localhost", port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY %s ORDER BY id ASC"""
-                , (argv[4], ))
+                LIKE BINARY %s ORDER BY id ASC""",
+                (argv[4], ))
     content = cur.fetchall()
     for i in content:
         print("{}".format(i))
