ff3989cbd57e64310b970102437564a956b8e27d
JuanJoseGomezR <juangomez0149@gmail.com>
2020-08-08 22:53:18 +0000

    [Uploading] 11th task

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""State Model"""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    """ not executed when imported """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    entry = State(name="Louisiana")
+    session.add(entry)
+    session.commit()
+    print(entry.id)
+    session.close()
