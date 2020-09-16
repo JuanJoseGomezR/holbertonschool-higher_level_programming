ad9c3c72ad023241857071dfb4c5d07bdc419ba7
JuanJoseGomezR <juangomez0149@gmail.com>
2020-09-16 03:06:14 +0000

    [Uploading] Tasks

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let a = process.argv[2];
-let b = process.argv[3];
+const a = process.argv[2];
+const b = process.argv[3];
 function add (a, b) {
   return (parseInt(a) + parseInt(b));
 }
